5645da86d6b91609191f07bcb37274f88889930b
Mike <42130525+vvvvtrt@users.noreply.github.com>
2024-04-06 12:56:26 +0300

    Update generate-STE.py

--- ml/generate-STE.py
+++ ml/generate-STE.py
@@ -8,11 +8,11 @@ NAME_MODEL = "cointegrated/rut5-base-multitask"
 tokenizer = T5Tokenizer.from_pretrained(NAME_MODEL)
 model = T5ForConditionalGeneration.from_pretrained(NAME_MODEL)
 
-def generate(text, **kwargs):
+def generate(text):
     inputs = tokenizer(text, return_tensors='pt')
 
     with torch.no_grad():
-        hypotheses = model.generate(**inputs, num_beams=5, **kwargs)
+        hypotheses = model.generate(**inputs, num_beams=5)
 
     return tokenizer.decode(hypotheses[0], skip_special_tokens=True)
 
