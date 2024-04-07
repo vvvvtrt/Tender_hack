353c6e3c2c98980d0a22759004d08d22979e3979
Mike <42130525+vvvvtrt@users.noreply.github.com>
2024-04-07 12:59:02 +0300

    Update nlp_search.py

--- ml/nlp_search.py
+++ ml/nlp_search.py
@@ -5,12 +5,17 @@ from transformers import T5ForConditionalGeneration, T5Tokenizer
 import requests
 import pymorphy2
 
-
 NAME_MODEL = "classification_model"
+NAME_MODEL2 = "generate_model"
 
 tokenizer = T5Tokenizer.from_pretrained(NAME_MODEL)
 model = T5ForConditionalGeneration.from_pretrained(NAME_MODEL)
+
+tokenizer1 = T5Tokenizer.from_pretrained(NAME_MODEL2)
+model1 = T5ForConditionalGeneration.from_pretrained(NAME_MODEL2)
+
 morph = pymorphy2.MorphAnalyzer()
+
 
 class NLP_Search:
     def __init__(self, text: str):
@@ -23,10 +28,10 @@ class NLP_Search:
     def search_all(self) -> dict:
         ans_dict = {}
 
-        ans_dict["name"] = ""
-        ans_dict["categories"] = self.classification()
+        ans_dict["update_name"] = self.genirate()
+        ans_dict["type"] = self.classification()
         ans_dict["country"] = self.search_country()
-        ans_dict["company"] = self.search_company()
+        ans_dict["producer"] = self.search_company()
         ans_dict["model"] = self.search_model(ans_dict["company"])
         ans_dict["units"] = self.search_units()
 
@@ -57,51 +62,50 @@ class NLP_Search:
         return suggestions
 
     def recursion(self, s, ttl=2):
-          if not ttl:
-              return []
+        if not ttl:
+            return []
 
-          for i in self.stop_world:
-              if i in s:
-                  return []
+        for i in self.stop_world:
+            if i in s:
+                return []
 
-          suggestions = self.get_suggestions(s)
-          # print(suggestions)
-          ans = [s] + suggestions
-          for i in suggestions:
-              ans += self.recursion(i, ttl - 1)
+        suggestions = self.get_suggestions(s)
+        # print(suggestions)
+        ans = [s] + suggestions
+        for i in suggestions:
+            ans += self.recursion(i, ttl - 1)
 
-          return ans
+        return ans
 
     def search_model(self, company: str) -> str:
-          tir = [set() for i in range(15)]
+        tir = [set() for i in range(15)]
 
-          for i in self.recursion(company.lower()):
-              new = i.split()
+        for i in self.recursion(company.lower()):
+            new = i.split()
 
-              for j in range(len(new)):
-                  tir[j].add(new[j])
+            for j in range(len(new)):
+                tir[j].add(new[j])
 
+        text = self.text.lower()
+        text_n = list(text.split())
+        for i in range(len(text_n)):
+            text_n.append("" if self.initial(text_n[i]) == "none" else self.initial(text_n[i]))
 
-          text = self.text.lower()
-          text_n = list(text.split())
-          for i in range(len(text_n)):
-              text_n.append("" if self.initial(text_n[i]) == "none" else self.initial(text_n[i]))
+        ans = ""
+        for level in range(len(tir)):
+            for i in tir[level]:
+                sec = self.initial(i)
 
-          ans = ""
-          for level in range(len(tir)):
-              for i in tir[level]:
-                  sec = self.initial(i)
+                if i in text_n and i not in ans:
+                    ans += i + " "
+                    break
 
-                  if i in text_n and i not in ans:
-                      ans += i + " "
-                      break
-                  
-                  if sec in text_n and i not in ans:
-                      ans += sec + " "
-                      break
+                if sec in text_n and i not in ans:
+                    ans += sec + " "
+                    break
 
-          ans = ans.replace(company.lower(), "")
-          return ans
+        ans = ans.replace(company.lower(), "")
+        return ans
 
     def initial(self, word):
         parsed_word = morph.parse(word)[0]
@@ -109,7 +113,6 @@ class NLP_Search:
             normal_form = parsed_word.normal_form
             return normal_form
         return "none"
-
 
     def search_country(self) -> str:
         ans = ""
@@ -153,6 +156,13 @@ class NLP_Search:
         return tokenizer.decode(hypotheses[0], skip_special_tokens=True)
 
 
+    def genirate(self):
+        inputs = tokenizer(self.text, return_tensors='pt')
+
+        with torch.no_grad():
+            hypotheses = model.generate(**inputs, num_beams=5)
+
+        return tokenizer.decode(hypotheses[0], skip_special_tokens=True)
 
 
 if __name__ == '__main__':
